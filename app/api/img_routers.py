54ac95e77c88ac64fb24d0fe751c8ce383fba9c7
Hansen-G <guohansen0010@gmail.com>
2022-08-22 17:08:56 -0400

    Change current user page

--- app/api/img_routers.py
+++ app/api/img_routers.py
@@ -4,25 +4,43 @@ from flask_login import current_user, login_user, logout_user, login_required
 
 img_routes = Blueprint('images', __name__)
 
-# Get all images home page by the current user
-@img_routes.route('/')
+# Get all images home page of the current user
+@img_routes.route('/current_user_images', methods=['GET'])
 @login_required
 def get_images():
-    
     id = current_user.id
-    all_images = Image.query.filter(Image.user_id == id).order_by(Image.createdAt.desc()).all()
-
+    all_images = Image.query.filter(Image.user_id == id).order_by(
+        Image.createdAt.desc()).all()
     return_JSON = ([i.to_dict() for i in all_images])
-
-
     for i in return_JSON:
         for j in i["liked_user_ids"]:
+            i['post_user'] = User.query.get(i['user_id']).to_dict()
             if j["id"] == current_user.id:
                 i['curent_user_liked'] = True
-                break
             else:
                 i['curent_user_liked'] = False
-              
+                
 
     return jsonify(return_JSON)
+
+# Get all images home page for the current user
+# @img_routes.route('/')
+# @login_required
+# def get_images_homepage():   
+#     id = current_user.id
+#     following_id = User.query.get(id)
+
+#     print('!!!!!!!!!!!!!!following_id', following_id.to_dict())
+#     # all_images = Image.query.filter(Image.user_id == id).order_by(Image.createdAt.desc()).all()
+#     # return_JSON = ([i.to_dict() for i in all_images])
+#     # for i in return_JSON:
+#     #     for j in i["liked_user_ids"]:
+#     #         if j["id"] == current_user.id:
+#     #             i['curent_user_liked'] = True
+#     #             break
+#     #         else:
+#     #             i['curent_user_liked'] = False
+              
+#     return 'HI'
+    #return jsonify(return_JSON)
     
--- app/models/following.py
+++ app/models/following.py
@@ -3,7 +3,7 @@ from .db import db
 follows = db.Table(
     "follows",
     db.Column("follower_id", db.Integer, db.ForeignKey("users.id")),
-    db.Column("followed_id", db.Integer, db.ForeignKey("users.id"))
+    db.Column("following_id", db.Integer, db.ForeignKey("users.id"))
 )
 
 
