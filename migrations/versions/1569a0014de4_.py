fbcc4efab74fe9173d4daac4049a5f94d0312659
Hui Feng <huifeng248@gmail.com>
2022-08-21 18:55:19 -0700

    seed data2

--- migrations/versions/1569a0014de4_.py
+++ migrations/versions/1569a0014de4_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 97bb467dae5c
+Revision ID: 1569a0014de4
 Revises: 
-Create Date: 2022-08-21 17:55:34.777998
+Create Date: 2022-08-21 18:54:10.234954
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '97bb467dae5c'
+revision = '1569a0014de4'
 down_revision = None
 branch_labels = None
 depends_on = None
