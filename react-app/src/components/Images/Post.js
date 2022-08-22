d25ff4e8396b27b8f0d55a12c880adbf3ceb393f
Jon Aguilar <technicianaguilar@gmail.com>
2022-08-22 15:38:40 -0700

    Got basic layout setup for profile

--- react-app/src/components/Images/Post.js
+++ react-app/src/components/Images/Post.js
@@ -4,11 +4,12 @@ import "./css/ProfilePost.css";
 const Post = ({ post }) => {
 	return (
 		<div className="single-post-container">
-			<div className="">
-				<Link to="#">
-					<img src={post.url} className="profile-post"></img>
-				</Link>
-			</div>
+
+
+			<Link to="#">
+				<img src={post.url} className="profile-post"></img>
+			</Link>
+
 		</div>
 	);
 };
