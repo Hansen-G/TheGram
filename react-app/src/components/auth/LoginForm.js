b1c9fb080fa63bb167e0be3b8395a87b9763b498
Jon Aguilar <technicianaguilar@gmail.com>
2022-08-23 16:49:25 -0700

    Styled login page, and implemented phone with image roulette

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -1,15 +1,24 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { login } from '../../store/session';
-
+import './LoginSignupForm.css'
 const LoginForm = () => {
   const [errors, setErrors] = useState([]);
   const [email, setEmail] = useState('');
+  const [index, setIndex] = useState(0)
+  const [url, setUrl] = useState('https://www.instagram.com/static/images/homepage/screenshots/screenshot4.png/a4fd825e3d49.png')
   const [password, setPassword] = useState('');
   const user = useSelector(state => state.session.user);
   const dispatch = useDispatch();
 
+  const imageArr = [
+    'https://www.instagram.com/static/images/homepage/screenshots/screenshot4.png/a4fd825e3d49.png',
+    'https://www.instagram.com/static/images/homepage/screenshots/screenshot3.png/94edb770accf.png',
+    'https://www.instagram.com/static/images/homepage/screenshots/screenshot2.png/4d62acb667fb.png',
+    'https://www.instagram.com/static/images/homepage/screenshots/screenshot1.png/fdfe239b7c9f.png'
+
+  ]
   const onLogin = async (e) => {
     e.preventDefault();
     const data = await dispatch(login(email, password));
@@ -18,6 +27,23 @@ const LoginForm = () => {
       setErrors(data);
     }
   };
+
+
+  useEffect(() => {
+
+    let clean = setInterval(() => {
+      console.log('roulette fire off', index)
+      if (index === 3) {
+        setIndex(0)
+      } else {
+        setIndex(index => index + 1)
+      }
+      console.log(imageArr[index])
+      setUrl(imageArr[index])
+      return url
+    }, 5000)
+    return () => clearInterval(clean)
+  }, [index])
 
   const updateEmail = (e) => {
     setEmail(e.target.value);
@@ -32,34 +58,51 @@ const LoginForm = () => {
   }
 
   return (
-    <form onSubmit={onLogin}>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
+    <div className='master-container'>
+      <div className='phone-anchor'>
+
+          <div className='phone'>
+          <img className='roulette' src={url} />
+          <img src='https://www.instagram.com/static/images/homepage/phones/home-phones.png/1dc085cdb87d.png' />
+          </div>
       </div>
-      <div>
-        <label htmlFor='email'>Email</label>
-        <input
-          name='email'
-          type='text'
-          placeholder='Email'
-          value={email}
-          onChange={updateEmail}
-        />
+
+      <div className='master-form-container'>
+        <div>
+          <img className='' src='https://www.instagram.com/static/images/web/logged_out_wordmark.png/7a252de00b20.png' />
+        </div>
+        <div>
+          <form onSubmit={onLogin}>
+            <div>
+              {errors.map((error, ind) => (
+                <div key={ind}>{error}</div>
+              ))}
+            </div>
+            <div>
+              {/* <label htmlFor='email'>Email</label> */}
+              <input
+                name='email'
+                type='text'
+                placeholder='Email'
+                value={email}
+                onChange={updateEmail}
+              />
+            </div>
+            <div>
+              {/* <label htmlFor='password'>Password</label> */}
+              <input
+                name='password'
+                type='password'
+                placeholder='Password'
+                value={password}
+                onChange={updatePassword}
+              />
+            </div>
+            <button type='submit'>Login</button>
+          </form>
+        </div>
       </div>
-      <div>
-        <label htmlFor='password'>Password</label>
-        <input
-          name='password'
-          type='password'
-          placeholder='Password'
-          value={password}
-          onChange={updatePassword}
-        />
-        <button type='submit'>Login</button>
-      </div>
-    </form>
+    </div>
   );
 };
 
