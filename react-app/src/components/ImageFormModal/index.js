0112c2cf45a0266eca14ff616ae06187ca236d82
Hui Feng <huifeng248@gmail.com>
2022-08-23 14:38:51 -0700

    imgae modal create and edit

--- react-app/src/components/ImageFormModal/index.js
+++ react-app/src/components/ImageFormModal/index.js
@@ -3,7 +3,24 @@ import {Modal} from '../../context/Modal'
 import ImageForm from './ImageForm'
 import './ImageForm.css'
 
-function ImageFormModal({action, })
+function ImageFormModal({action, imageId, showModal, setShowModal}) {
+    // const [showModal, setShowModal] = useState(true)
+
+    return (
+        <div>
+            {/* <button className='create_a_image_button' >
+                {action}
+            </button> */}
+            {showModal && (
+                // <div>hihihi </div>
+                <Modal onClose={()=> setShowModal(false)}>
+                    <div>hihihi</div>
+                    <ImageForm action={action} imageId={imageId} setShowModal={setShowModal} showModal={showModal} />
+                </Modal>
+            )}
+        </div>
+    )
+}
 
 
 
