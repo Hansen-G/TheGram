dcb586acca5e7774b294d20fbbbb9768b01a1141
yonilurie <yonilurie@protonmail.com>
2022-08-23 21:59:07 -0700

    Added more styling to login form

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -1,109 +1,128 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Redirect } from 'react-router-dom';
-import { login } from '../../store/session';
-import './LoginSignupForm.css'
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link, Redirect } from "react-router-dom";
+import { login } from "../../store/session";
+import "./LoginSignupForm.css";
 const LoginForm = () => {
-  const [errors, setErrors] = useState([]);
-  const [email, setEmail] = useState('');
-  const [index, setIndex] = useState(0)
-  const [url, setUrl] = useState('https://www.instagram.com/static/images/homepage/screenshots/screenshot1.png/fdfe239b7c9f.png')
-  const [password, setPassword] = useState('');
-  const user = useSelector(state => state.session.user);
-  const dispatch = useDispatch();
+	const [errors, setErrors] = useState([]);
+	const [email, setEmail] = useState("");
+	const [index, setIndex] = useState(0);
+	const [url, setUrl] = useState(
+		"https://www.instagram.com/static/images/homepage/screenshots/screenshot1.png/fdfe239b7c9f.png"
+	);
+	const [password, setPassword] = useState("");
+	const user = useSelector((state) => state.session.user);
+	const dispatch = useDispatch();
 
-  const imageArr = [
-    'https://www.instagram.com/static/images/homepage/screenshots/screenshot4.png/a4fd825e3d49.png',
-    'https://www.instagram.com/static/images/homepage/screenshots/screenshot3.png/94edb770accf.png',
-    'https://www.instagram.com/static/images/homepage/screenshots/screenshot2.png/4d62acb667fb.png',
-    'https://www.instagram.com/static/images/homepage/screenshots/screenshot1.png/fdfe239b7c9f.png'
+	const imageArr = [
+		"https://www.instagram.com/static/images/homepage/screenshots/screenshot4.png/a4fd825e3d49.png",
+		"https://www.instagram.com/static/images/homepage/screenshots/screenshot3.png/94edb770accf.png",
+		"https://www.instagram.com/static/images/homepage/screenshots/screenshot2.png/4d62acb667fb.png",
+		"https://www.instagram.com/static/images/homepage/screenshots/screenshot1.png/fdfe239b7c9f.png",
+	];
+	const onLogin = async (e) => {
+		e.preventDefault();
+		const data = await dispatch(login(email, password));
+		if (data) {
+			console.log("@@@@", data);
+			setErrors(data);
+		}
+	};
 
-  ]
-  const onLogin = async (e) => {
-    e.preventDefault();
-    const data = await dispatch(login(email, password));
-    if (data) {
-      console.log("@@@@", data)
-      setErrors(data);
-    }
-  };
+	useEffect(() => {
+		let clean = setInterval(() => {
+			console.log("roulette fire off", index);
+			if (index === 3) {
+				setIndex(0);
+			} else {
+				setIndex((index) => index + 1);
+			}
+			console.log(imageArr[index]);
+			setUrl(imageArr[index]);
+			return url;
+		}, 5000);
+		return () => clearInterval(clean);
+	}, [index]);
 
+	const updateEmail = (e) => {
+		setEmail(e.target.value);
+	};
 
-  useEffect(() => {
+	const updatePassword = (e) => {
+		setPassword(e.target.value);
+	};
 
-    let clean = setInterval(() => {
-      console.log('roulette fire off', index)
-      if (index === 3) {
-        setIndex(0)
-      } else {
-        setIndex(index => index + 1)
-      }
-      console.log(imageArr[index])
-      setUrl(imageArr[index])
-      return url
-    }, 5000)
-    return () => clearInterval(clean)
-  }, [index])
+	if (user) {
+		return <Redirect to="/" />;
+	}
 
-  const updateEmail = (e) => {
-    setEmail(e.target.value);
-  };
-
-  const updatePassword = (e) => {
-    setPassword(e.target.value);
-  };
-
-  if (user) {
-    return <Redirect to='/' />;
-  }
-
-  return (
-    <div className='master-container'>
-      <div className='phone-anchor'>
-
-          <div className='phone'>
-          <img className='roulette' src={url} />
-          <img src='https://www.instagram.com/static/images/homepage/phones/home-phones.png/1dc085cdb87d.png' />
-          </div>
-      </div>
-
-      <div className='master-form-container'>
-        <div>
-          <img className='' src='https://www.instagram.com/static/images/web/logged_out_wordmark.png/7a252de00b20.png' />
-        </div>
-        <div>
-          <form onSubmit={onLogin}>
-            <div>
-              {errors.map((error, ind) => (
-                <div key={ind}>{error}</div>
-              ))}
-            </div>
-            <div>
-              {/* <label htmlFor='email'>Email</label> */}
-              <input
-                name='email'
-                type='text'
-                placeholder='Email'
-                value={email}
-                onChange={updateEmail}
-              />
-            </div>
-            <div>
-              {/* <label htmlFor='password'>Password</label> */}
-              <input
-                name='password'
-                type='password'
-                placeholder='Password'
-                value={password}
-                onChange={updatePassword}
-              />
-            </div>
-            <button type='submit'>Login</button>
-          </form>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="master-container">
+			<div className="phone-anchor">
+				<div className="phone">
+					<img className="roulette" src={url} />
+					<img src="https://www.instagram.com/static/images/homepage/phones/home-phones.png/1dc085cdb87d.png" />
+				</div>
+			</div>
+			<div>
+				<div className="master-form-container">
+					<div className="lrg-logo-container">
+						<img
+							className=""
+							src="https://www.instagram.com/static/images/web/logged_out_wordmark.png/7a252de00b20.png"
+						/>
+					</div>
+					<div>
+						<form onSubmit={onLogin}>
+							<div>
+								{errors.map((error, ind) => (
+									<div key={ind}>{error}</div>
+								))}
+							</div>
+							<div>
+								{/* <label htmlFor='email'>Email</label> */}
+								<input
+									name="email"
+									type="text"
+									placeholder="Email"
+									value={email}
+									onChange={updateEmail}
+									className="verification-form-input"
+								/>
+							</div>
+							<div>
+								{/* <label htmlFor='password'>Password</label> */}
+								<input
+									name="password"
+									type="password"
+									placeholder="Password"
+									value={password}
+									onChange={updatePassword}
+									className="verification-form-input"
+								/>
+							</div>
+							<button type="submit" className="submit-btn">
+								Log In
+							</button>
+						</form>
+						<div className="or-container">
+							<div className="line"></div>
+							<div className="or">OR</div>
+							<div className="line"></div>
+						</div>
+					</div>
+				</div>
+				<div className="switch-method">
+					<div className="switch-method-content">
+						Don't have an account?{" "}
+						<Link to={"/sign-up"} className="switch-method-lnk">
+							Sign Up
+						</Link>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default LoginForm;
