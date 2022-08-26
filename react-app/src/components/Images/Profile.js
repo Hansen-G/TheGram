7ac35ab6d17dc06ac1f2477700a7230cf17ce8f0
yonilurie <yonilurie@protonmail.com>
2022-08-26 10:06:46 -0700

    Moved function into conditional block and changed class to className

--- react-app/src/components/Images/Profile.js
+++ react-app/src/components/Images/Profile.js
@@ -42,10 +42,11 @@ const Profile = () => {
 		}
 		if (Number(userId)) {
 			dispatch(loadImages(userId));
+			getUser();
 		}
 		// dispatch(loadImages(userId));
 		//Call backend
-		getUser();
+		
 	}, [dispatch, userId]);
 
 	return (
@@ -77,7 +78,7 @@ const Profile = () => {
 										>
 											<svg
 												aria-label="Following"
-												class="_ab6-"
+												className="ab6-"
 												color="#262626"
 												fill="#262626"
 												height="15"
