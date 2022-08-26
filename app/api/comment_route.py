11c47ea3de127798426754a61e1490966ddc0312
yonilurie <yonilurie@protonmail.com>
2022-08-25 18:58:05 -0700

    Accepted extra incoming change

--- app/api/comment_route.py
+++ app/api/comment_route.py
@@ -72,29 +72,6 @@ def update_comment(id):
 @comment_routes.route('/<int:id>/likes', methods=['POST'])
 @login_required
 def add_like_to_image(id):
-<<<<<<< HEAD
-    form = FormValidation()
-    form['csrf_token'].data = request.cookies['csrf_token']
-    if form.validate_on_submit():
-        comment = Comment.query.get(id).to_dict()
-        current_user_id = current_user.id
-        for user in comment['user_comment_likes']:
-            user = user.to_dict()
-            if current_user_id == user['id']:
-                
-                deleted_like = delete(CommentsLikes).where(
-                    CommentsLikes.c.user_id == current_user_id,
-                    CommentsLikes.c.comment_id == id
-                )
-                db.engine.execute(deleted_like)
-                return f'unlike comment {id}'
-
-        new_like = CommentsLikes.insert().values((current_user_id, id))
-        db.engine.execute(new_like)
-        return f'like comment {id}'
-    else:
-        return jsonify(form.errors)
-=======
     comment = Comment.query.get(id).to_dict()
     current_user_id = current_user.id
     for user in comment['user_comment_likes']:
@@ -111,4 +88,3 @@ def add_like_to_image(id):
     new_like = CommentsLikes.insert().values((current_user_id, id))
     db.engine.execute(new_like)
     return jsonify(comment)
->>>>>>> Hansen-G/comment-edit
