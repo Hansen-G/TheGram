494aa1d92833e482f047df66964c5de6f32020e0
yonilurie <yonilurie@protonmail.com>
2022-10-07 12:45:31 -0700

    Changes filefield to stringfield

--- app/forms/image_form.py
+++ app/forms/image_form.py
@@ -7,7 +7,7 @@ from wtforms.validators import DataRequired, Email, ValidationError, url, Length
 
 class ImageForm(FlaskForm):
     # url = StringField("Url", validators=[DataRequired(), url(), Length(min=1, max=250)])
-    image = FileField('image')
+    image = StringField('image')
     description = StringField("Description", validators=[Length(max=2200)])
     alt_description = StringField("ALT Description", validators=[Length(max=2200)])
     show_stats = StringField("Show Status", default=True, validators=[DataRequired()])
